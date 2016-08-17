10f329f9eeef2b98c0641be4a02c5d35105b91c5
Josiah Hunsinger <fencepost13302@gmail.com>
2016-08-17 09:21:17 -0500

    made python section pip-installable

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='ledcube',
+    version='0.1.0',
+    author='Josiah Hunsinger',
+    author_email='josiah.hunsinger@gmail.com',
+    url='https://github.com/aiguy1010/ledcube',
+    py_modules=['ledcube'],
+    install_requires=['pyserial']
+)
